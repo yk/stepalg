632eb5d98078a24f6257b0b4dfcebcaac25dabbb
Yannic Kilcher <yannic.kilcher@gmail.com>
2015-11-18 20:04:43 +0100

    changed evaluation

--- python2_noedit/stepalg.py
+++ python2_noedit/stepalg.py
@@ -4,24 +4,35 @@ from typing import Dict, Any, Callable
 
 class StepwiseAlgorithm(object):
     def initialize(self, state):
+        u"""
+        :param state: this will be the same for the whole algorithm. use it as stateful memory
+        :return:
+        """
         pass
 
-    def step(self, current_step, state):
-        pass
-
-    def evaluate(self, current_step, state):
+    def step(self, current_step, state, evaluate):
+        u"""
+        :param current_step:
+        :param state:
+        :param evaluate: when this is true, you should evaluate the current iterate
+        :return: how many datapoints you have consumed. If None, then 1 is assumed
+        """
         pass
 
     def output(self, current_step, state):
+        u"""
+        it's best to return a dictionary with tuples of lists as values, such as
+        {
+            loss: (epochs, values)
+        }
+        """
         pass
 
 
-def as_stepwise_algorithm(initialize, step, evaluate,
-                          output):
+def as_stepwise_algorithm(initialize, step, output):
     return type(u"AdHocStepwiseAlgorithm", (StepwiseAlgorithm,), {
         u"initialize": lambda self, state: initialize(state),
-        u"step": lambda self, current_step, state: step(current_step, state),
-        u"evaluate": lambda self, current_step, state: evaluate(current_step, state),
+        u"step": lambda self, current_step, state, evaluate: step(current_step, state, evaluate),
         u"output": lambda self, current_step, state: output(current_step, state),
     })()
 
@@ -32,22 +43,22 @@ def run_stepwise_algorithm(algorithm, do_stop,
     algorithm.initialize(state)
     current_step = 0
     last_eval = 0
-    algorithm.evaluate(current_step, state)
 
     while not do_stop(current_step, state):
-        steps_taken = algorithm.step(current_step, state)
-        current_step += steps_taken
-
+        evaluate = False
         if do_evaluate(current_step, last_eval, state):
             last_eval = current_step
-            algorithm.evaluate(current_step, state)
-
-    if last_eval < current_step:
-        algorithm.evaluate(current_step, state)
+            evaluate = True
+        steps_taken = algorithm.step(current_step, state, evaluate)
+        if steps_taken is None:
+            steps_taken = 1
+        current_step += steps_taken
 
     return algorithm.output(current_step, state)
 
 
 def run_stepwise_algorithm_basic(algorithm, total_steps, evaluate_after):
     return run_stepwise_algorithm(algorithm, lambda current_step, state: current_step >= total_steps,
-                                  lambda current_step, last_eval, state: last_eval + evaluate_after <= current_step)
+                                  lambda current_step, last_eval,
+                                         state: last_eval + evaluate_after <= current_step or current_step == 0)
+
--- stepalg/stepalg.py
+++ stepalg/stepalg.py
@@ -3,24 +3,35 @@ from typing import Dict, Any, Callable
 
 class StepwiseAlgorithm:
     def initialize(self, state: Dict[str, Any]):
+        """
+        :param state: this will be the same for the whole algorithm. use it as stateful memory
+        :return:
+        """
         pass
 
-    def step(self, current_step: int, state: Dict[str, Any]) -> int:
-        pass
-
-    def evaluate(self, current_step: int, state: Dict[str, Any]):
+    def step(self, current_step: int, state: Dict[str, Any], evaluate) -> int:
+        """
+        :param current_step:
+        :param state:
+        :param evaluate: when this is true, you should evaluate the current iterate
+        :return: how many datapoints you have consumed. If None, then 1 is assumed
+        """
         pass
 
     def output(self, current_step: int, state: Dict[str, Any]):
+        """
+        it's best to return a dictionary with tuples of lists as values, such as
+        {
+            loss: (epochs, values)
+        }
+        """
         pass
 
 
-def as_stepwise_algorithm(initialize: Callable, step: Callable, evaluate: Callable,
-                          output: Callable) -> StepwiseAlgorithm:
+def as_stepwise_algorithm(initialize: Callable, step: Callable, output: Callable) -> StepwiseAlgorithm:
     return type("AdHocStepwiseAlgorithm", (StepwiseAlgorithm,), {
         "initialize": lambda self, state: initialize(state),
-        "step": lambda self, current_step, state: step(current_step, state),
-        "evaluate": lambda self, current_step, state: evaluate(current_step, state),
+        "step": lambda self, current_step, state, evaluate: step(current_step, state, evaluate),
         "output": lambda self, current_step, state: output(current_step, state),
     })()
 
@@ -31,22 +42,22 @@ def run_stepwise_algorithm(algorithm: StepwiseAlgorithm, do_stop: Callable[[int,
     algorithm.initialize(state)
     current_step = 0
     last_eval = 0
-    algorithm.evaluate(current_step, state)
 
     while not do_stop(current_step, state):
-        steps_taken = algorithm.step(current_step, state)
-        current_step += steps_taken
-
+        evaluate = False
         if do_evaluate(current_step, last_eval, state):
             last_eval = current_step
-            algorithm.evaluate(current_step, state)
-
-    if last_eval < current_step:
-        algorithm.evaluate(current_step, state)
+            evaluate = True
+        steps_taken = algorithm.step(current_step, state, evaluate)
+        if steps_taken is None:
+            steps_taken = 1
+        current_step += steps_taken
 
     return algorithm.output(current_step, state)
 
 
 def run_stepwise_algorithm_basic(algorithm: StepwiseAlgorithm, total_steps: int, evaluate_after: int):
     return run_stepwise_algorithm(algorithm, lambda current_step, state: current_step >= total_steps,
-                                  lambda current_step, last_eval, state: last_eval + evaluate_after <= current_step)
+                                  lambda current_step, last_eval,
+                                         state: last_eval + evaluate_after <= current_step or current_step == 0)
+
